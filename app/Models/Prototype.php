ed872a8a487a0559109c26c862a8644a7c025c6e
SebastianMay2004 <2202049@utrivieramaya.edu.mx>
2025-10-23 15:36:24 -0500

    model, migration of prototype

---
+++ app/Models/Prototype.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use app\Http\Controllers\PrototypeController;
+
+class Prototype extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prototype';
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'temperature',
+        'humidity',
+        'dust_level',
+    ];
+
+    protected $casts = [
+        'temperature' => 'float',
+        'humidity' => 'float',
+        'dust_level' => 'float',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
